2b36df34723f9673b2172226f563dea37ec4b644
djzib0 <piotrsabatura@gmail.com>
2024-11-07 18:06:43 +0100

    updated project to new next.js version. adjusted styles

--- src/components/budgetsList/budgetLink/BudgetLink.tsx
+++ src/components/budgetsList/budgetLink/BudgetLink.tsx
@@ -17,50 +17,49 @@ const BudgetLink = ({budget, linkTo} : {budget: BudgetType; linkTo: string}) => 
      >
       <div className={styles.budgetLinkContainer}>
         <div className={styles.budgetLinkTitle}>
-          <h2 className={styles.monthTitle}>
-            {`${convertToMonthName(parseInt(convertBudgetNameToMonth((budget.budgetName))), "En-en", MonthNameLength.Long)}`}</h2>
-          <h2 className={styles.yearTitle}>
-            {`${convertBudgetNameToYear(budget.budgetName)}`}
+          <h2>
+            {
+            `${convertBudgetNameToYear(budget.budgetName)} 
+            ${convertToMonthName(parseInt(convertBudgetNameToMonth((budget.budgetName))), "En-en", MonthNameLength.Long)}
+            `}
           </h2>
         </div>
-        <div className={styles.budgetLinkDetails}>
-          <div className={styles.budgetSumContainer}>
-            <p>Total: </p>
-            <p>{sumBudget(budget)}</p>
+        <div className={styles.budgetSumContainer}>
+          <p>Budget: </p>
+          <p>{sumBudget(budget)}</p>
+        </div>
+        <div className={styles.budgetDetails}>
+          <div className={styles.budgetDetailsElement}>
+            <p>Food:</p>
+            <p>{budgetSummary.foodBudget}</p>
           </div>
-          <div className={styles.budgetDetails}>
-            <div className={styles.budgetDetailsElement}>
-              <p>Food:</p>
-              <p>{budgetSummary.foodBudget}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Health:</p>
-              <p>{budgetSummary.healthBudget}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Transport:</p>
-              <p>{budgetSummary.transportBudget}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Clothes:</p>
-              <p>{budgetSummary.clothesBudget}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Home:</p>
-              <p>{budgetSummary.homeBudget}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Digital:</p>
-              <p>{budgetSummary.digitalServices}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Hobby:</p>
-              <p>{budgetSummary.hobbyBudget}</p>
-            </div>
-            <div className={styles.budgetDetailsElement}>
-              <p>Other:</p>
-              <p>{budgetSummary.otherBudget}</p>
-            </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Health:</p>
+            <p>{budgetSummary.healthBudget}</p>
+          </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Transport:</p>
+            <p>{budgetSummary.transportBudget}</p>
+          </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Clothes:</p>
+            <p>{budgetSummary.clothesBudget}</p>
+          </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Home:</p>
+            <p>{budgetSummary.homeBudget}</p>
+          </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Digital:</p>
+            <p>{budgetSummary.digitalServices}</p>
+          </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Hobby:</p>
+            <p>{budgetSummary.hobbyBudget}</p>
+          </div>
+          <div className={styles.budgetDetailsElement}>
+            <p>Other:</p>
+            <p>{budgetSummary.otherBudget}</p>
           </div>
         </div>
       </div>
