5dc083351fddb4142fa9a1590f11355188a9d633
djzib0 <piotrsabatura@gmail.com>
2024-11-16 21:40:51 +0100

    WIP: adding styles to login form

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -5,7 +5,9 @@ import { login} from "@/lib/actions";
 import { useRouter } from "next/navigation"
 import { useActionState, useEffect } from "react"
 import Link from "next/link";
-
+// icons import
+import { ImFacebook2 } from "react-icons/im";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginForm = () => {
 
@@ -26,6 +28,19 @@ const LoginForm = () => {
         <input type='password' placeholder='password' name='password' />
         <button className={styles.btn}>Login</button> 
       </form>
+      <div className={styles.providersLoginContainer}>
+        <p>Or login with</p>
+        <div className={styles.providersBtnsContainer}>
+          <button className={styles.providerBtn} id={styles.facebookLoginBtn}>
+            <div className={styles.providerBtnIcon}><ImFacebook2 /></div>
+            <div className={styles.providerBtnText}>Facebook</div>
+          </button>
+          <button className={styles.providerBtn} id={styles.googleLoginBtn}>
+            <div className={styles.providerBtnIcon}><FcGoogle /></div>
+            <div className={styles.providerBtnText}>Google</div>
+          </button>
+        </div>
+      </div>
       <div className={styles.registerContainer}>
         <p>Not a member?</p>
         <Link href="/register" className={styles.link}>
